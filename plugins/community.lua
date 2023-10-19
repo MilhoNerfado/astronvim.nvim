0ef484c5b85d7c2a7ea69cb096f80ee41c612444
Emilio Bottoni <emiliobottoni02@gmail.com>
2023-10-19 15:30:08 -0300

    Add languages and some management stuff

--- plugins/community.lua
+++ plugins/community.lua
@@ -3,11 +3,31 @@ return {
   "AstroNvim/astrocommunity",
   -- example of importing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
-
+    
+  -- Themes / colorschemes
   { import = "astrocommunity.colorscheme.catppuccin" },
 
+  -- Motions
   { import = "astrocommunity.motion.harpoon" },
+
+
+  -- Project related
+  { import = "astrocommunity.project.projectmgr-nvim" },
+
+  -- Languages
   { import = "astrocommunity.pack.python" },
   { import = "astrocommunity.pack.rust" },
-  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.pack.cmake" },
+  { import = "astrocommunity.pack.cpp" },
+  -- { import = "astrocommunity.pack.nix" },
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.markdown" },
+  -- { import = "astrocommunity.pack.haskell" },
+  -- { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.toml" },
+  { import = "astrocommunity.pack.bash" },
+  -- { import = "astrocommunity.pack.docker" },
+  -- { import = "astrocommunity.pack.html-css" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.yaml" },
 }
